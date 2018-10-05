de8113f3ddc5abf6f2fd724ca841768b57c462fd
Bouabacar <boubacarbah@ac.c4q.nyc>
2018-10-05 18:51:44 -0400

    finished implementation

--- app/src/main/java/nyc/bbah/mvp_retro/adapter/NoticeAdapter.java
+++ app/src/main/java/nyc/bbah/mvp_retro/adapter/NoticeAdapter.java
@@ -1,4 +1,65 @@
 package nyc.bbah.mvp_retro.adapter;
 
-public class NoticeAdapter {
+import android.annotation.SuppressLint;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import nyc.bbah.mvp_retro.R;
+import nyc.bbah.mvp_retro.main_activity.RecyclerItemClickListener;
+import nyc.bbah.mvp_retro.model.Notice;
+
+public class NoticeAdapter extends RecyclerView.Adapter<NoticeAdapter.EmployeeViewHolder> {
+
+    private ArrayList<Notice> dataList;
+    private RecyclerItemClickListener recyclerItemClickListener;
+
+    public NoticeAdapter(ArrayList<Notice> dataList , RecyclerItemClickListener recyclerItemClickListener) {
+        this.dataList = dataList;
+        this.recyclerItemClickListener = recyclerItemClickListener;
+    }
+
+
+    @Override
+    public EmployeeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.single_view_row, parent, false);
+        return new EmployeeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(EmployeeViewHolder holder, @SuppressLint("RecyclerView") final int position) {
+        holder.txtNoticeTitle.setText(dataList.get(position).getTitle());
+        holder.txtNoticeBrief.setText(dataList.get(position).getBrief());
+        holder.txtNoticeFilePath.setText(dataList.get(position).getFileSource());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recyclerItemClickListener.onItemClick(dataList.get(position));
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+    class EmployeeViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txtNoticeTitle, txtNoticeBrief, txtNoticeFilePath;
+
+        EmployeeViewHolder(View itemView) {
+            super(itemView);
+            txtNoticeTitle =  itemView.findViewById(R.id.txt_notice_title);
+            txtNoticeBrief =  itemView.findViewById(R.id.txt_notice_brief);
+            txtNoticeFilePath =  itemView.findViewById(R.id.txt_notice_file_path);
+
+        }
+    }
 }
